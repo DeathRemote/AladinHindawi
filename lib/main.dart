08255f0df497cfeee2208dfacae66f015a9db2ad
DeathRemote <61564684+DeathRemote@users.noreply.github.com>
2021-10-03 13:16:47 +0200

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'Screens/login_signup.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Login Signup UI',
+      home: LoginSignupScreen(),
+    );
+  }
+}
